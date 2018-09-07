49999304b834285327a5a62cd6cde8cc002c78b6
ludeeus <joasoe@gmail.com>
2018-09-07 19:59:28 +0200

    Introduced the features

---
+++ README.md
@@ -0,0 +1,47 @@
+# pyhassbian - A python module to interact with hassbian-config
+
+## Install
+
+```bash
+pip install pyhassbian
+```
+
+### Verify that hassbian-config is installed
+
+```python
+import pyhassbian
+pyhassbian.get_is_installed()
+# Sample output: True
+```
+
+### Get a list of all available suites
+
+```python
+import pyhassbian
+print(pyhassbian.get_suites())
+# Sample output: ['hue', 'samba', 'homeassistant']
+```
+
+### Get the version number of hassbian-config
+
+```python
+import pyhassbian
+print(pyhassbian.get_version())
+# Sample output: 0.9.3
+```
+
+### Install or upgrade a suite with hassbian-config
+
+```python
+import pyhassbian
+pyhassbian.manage_suite('install', 'hue')
+pyhassbian.manage_suite('upgrade', 'homeassistant', version='0.80.3')
+pyhassbian.manage_suite('upgrade', 'hassbian-script', dev=True)
+```
+
+### Upgrade the base OS
+
+```python
+import pyhassbian
+pyhassbian.os_upgrade()
+```
---
+++ pyhassbian/__init__.py
@@ -0,0 +1,78 @@
+"""
+A python module to interact with hassbian-config.
+
+This code is released under the terms of the MIT license. See the LICENSE
+file for more details.
+"""
+import subprocess
+import os
+from pathlib import Path
+
+
+def get_is_installed():
+    """Verify that hassbian-config is installed."""
+    if Path("/usr/local/bin/hassbian-config").is_file():
+        exist = True
+    else:
+        exist = False
+    return exist
+
+
+def get_suites():
+    """Get a list of available suites."""
+    suites = []
+    for file in os.listdir("/opt/hassbian/suites"):
+        if file.endswith(".sh"):
+            suites.append(file[:-3])
+    return suites
+
+
+def get_version():
+    """Return the version of hassbian-config."""
+    command = subprocess.Popen(['hassbian-config', '--version'],
+                               stdout=subprocess.PIPE)
+    out = command.communicate()[:-2]
+    version = str(out[0][:-1]).split("'")[1]
+    return version
+
+
+def manage_suite(mode, suite, dev=False, beta=False, version=None):
+    """Upgrade a suite with hassbian-config."""
+    if suite in get_suites():
+        if dev:
+            subprocess.call(['sudo',
+                             'hassbian-config',
+                             mode,
+                             suite,
+                             '--accept',
+                             '--dev'])
+        elif beta:
+            subprocess.call(['sudo',
+                             'hassbian-config',
+                             mode,
+                             suite,
+                             '--accept',
+                             '--beta'])
+        else:
+            if version:
+                subprocess.call(['sudo',
+                                 'hassbian-config',
+                                 mode,
+                                 suite + '=' + str(version),
+                                 '--accept'])
+            else:
+                subprocess.call(['sudo',
+                                 'hassbian-config',
+                                 mode,
+                                 suite,
+                                 '--accept'])
+
+
+def os_upgrade():
+    """Upgrade the base OS."""
+    subprocess.call(['sudo',
+                     'hassbian-config',
+                     'upgrade',
+                     'hassbian',
+                     '--accept',
+                     '--dev'])
