c8ad8f30e64a022c6a0151101c73b804a36c2ce4
ludeeus <joasoe@gmail.com>
2019-02-01 18:42:31 +0100

    adds log

--- pyhassbian/server.py
+++ pyhassbian/server.py
@@ -38,6 +38,23 @@ async def html(request):
     content += static.FOOTER
     return web.Response(body=content, content_type="text/html")
 
+async def log(request):
+    """Serve a HTML site."""
+    print("Session from:", request.headers.get('X-FORWARDED-FOR', None))
+    content = static.STYLE
+    version = 96
+    content += static.HEADER.format(version=version, previous=95, next=97)
+
+    content += '<main>'
+
+    lastlog = await get_log()
+
+    content = static.CARD.format(title='log', content=lastlog, docs='doclink')
+
+    content += '</main>'
+    content += static.FOOTER
+    return web.Response(body=content, content_type="text/html")
+
 async def json(request):
     """Serve the response as JSON."""
     json_data = await get_data()
@@ -48,9 +65,14 @@ async def get_data():
     """Get version data."""
     return Manager().get_suites()
 
+async def get_log():
+    """Get version data."""
+    return Manager().log()
+
 def run_server():
     """Run the server."""
     app = web.Application()
     app.router.add_route('GET', r'/', html, name='html')
+    app.router.add_route('GET', r'/log', html, name='log')
     app.router.add_route('GET', r'/json', json, name='json')
     web.run_app(app, port=9999)
