6ec808564e8328debaca6446313e387925c3bd02
ludeeus <joasoe@gmail.com>
2019-02-02 15:50:07 +0100

    that failed

--- pyhassbian/cli.py
+++ pyhassbian/cli.py
@@ -1,5 +1,4 @@
 """Enable CLI."""
-import asyncio
 import click
 
 
@@ -7,9 +6,8 @@ import click
 @click.option('--port', '-P', default=9999, help='port number.')
 def cli(port):
     """CLI for this package."""
-    loop = asyncio.get_event_loop()
     from pyhassbian.server import run_server
-    loop.run_until_complete(run_server(port))
+    run_server(port)
 
 
 cli()  # pylint: disable=E1120
