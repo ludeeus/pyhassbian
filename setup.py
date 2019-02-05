e353c287d79b0a1d17073f055dede6fc429640bb
ludeeus <joasoe@gmail.com>
2019-02-05 17:36:02 +0100

    fix dependecy

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ with open("README.md", "r") as fh:
     LONG = fh.read()
 setuptools.setup(
     name="pyhassbian",
-    version="0.2.1",
+    version="0.2.2",
     author="Joakim Sorensen",
     author_email="ludeeus@gmail.com",
     description="",
     long_description=LONG,
     long_description_content_type="text/markdown",
-    install_requires=['aiohttp', 'requests', 'click', 'aiohttp_basicauth'],
+    install_requires=['aiohttp', 'requests', 'click', 'aiohttp-basicauth'],
     url="https://github.com/ludeeus/pyhassbian",
     packages=setuptools.find_packages(),
     package_data={'': ['./static/*']},
