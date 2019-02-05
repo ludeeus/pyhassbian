6501a8cb0a9faeecf00d93d9fa4cba7665882099
ludeeus <joasoe@gmail.com>
2019-02-05 17:25:22 +0100

    Typo

--- pyhassbian/cli.py
+++ pyhassbian/cli.py
@@ -6,7 +6,7 @@ import click
 @click.option('--port', '-p', default=9999, help='Port number.')
 @click.option('--username', '-U', default='pi', help='Username.')
 @click.option('--password', '-P', default='raspberry', help='Password.')
-@click.option('--nu_auth', is_flag=True, help='Disable auth.')
+@click.option('--no_auth', is_flag=True, help='Disable auth.')
 def cli(port, username, password, no_auth):
     """CLI for this package."""
     from pyhassbian.server import run_server
